6237b19c350a8c834b7522e503596eb073277edb
FOXX <siliconfoxx@gmail.com>
2016-01-27 16:30:42 -0600

    Delete cf-failsafe.sh

--- cfengine/masterfiles-example-1/cf-failsafe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# NOTE:  This file rebuilds the cfengine configuration directory in the event it
-# becomes irreperably damaged using a backup directory tree.
-
-mkdir /var/cfengine
-cp -pr /var/cf-failsafe/* /var/cfengine
-/var/cfengine/bin/cf-agent && /var/cfengine/bin/cf-agent
