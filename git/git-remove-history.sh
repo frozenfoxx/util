0223b825ef6b5b4a2141000afde4cb72ba2d9fd7
FOXX <siliconfoxx@gmail.com>
2017-03-08 23:50:41 -0600

    Moved git-remove-history

--- git/git-remove-history.sh
+++ git/git-remove-history.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+#############################################################
+# Name:         git-delete-history.sh
+# Author(s):    FOXX (frozenfoxx@github.com), David Underhill
+# Description:  script to permanently delete files/folders
+#   from your git repository.  To use it, cd to your
+#   repository's root and then run the script with a list
+#   of paths you want to delete, e.g.,
+#   git-delete-history path1 path2
+############################################################
+
 set -o errexit
- 
-# Author: David Underhill
-# Script to permanently delete files/folders from your git repository.  To use 
-# it, cd to your repository's root and then run the script with a list of paths
-# you want to delete, e.g., git-delete-history path1 path2
  
 if [ $# -eq 0 ]; then
     exit 0are still
