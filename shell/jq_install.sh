2086ced26cf0364285be834a0fe72d84fead053e
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2018-10-05 20:47:40 +0000

    Update jq_install.sh

--- shell/jq_install.sh
+++ shell/jq_install.sh
@@ -4,36 +4,54 @@
 
 # Variables
 PLATFORM=$(uname -s)
+LOG_PATH=${LOG_PATH:-'/var/root/log'}
+STD_LOG='jq_install.log'
+STD_LOG_ARG=">>${LOG_PATH}/${STD_LOG}"
+
+if [ ${PLATFORM} == 'Darwin' ]; then
+  INSTALL_CMD="$(which brew) install"
+elif [ ${PLATFORM} == 'Linux' ]; then
+  INSTALL_CMD="$(which apt-get) update && $(which apt-get) install -y"
+else
+  eval echo "[!] This platform is not supported yet" ${STD_LOG_ARG}
+  exit 1
+fi
 
 # Functions
 
-# Install jq for OSX
-install_osx()
+# Install jq
+install()
 {
-  /usr/local/bin/brew install jq
+  eval echo "[+] Installing jq..." ${STD_LOG_ARG}
+  eval ${INSTALL_CMD} jq
 }
 
-# Install jq for Linux
-install_linux()
+# Nulls the logging arguments to turn off logging
+unset_logs()
 {
-  /usr/bin/apt-get update && /usr/bin/apt-get install -y jq
+  echo "[+] Running with no log option..."
+  STD_LOG_ARG=''
 }
 
-# Show usage information
+# Display usage information
 usage()
 {
-  echo "usage: jq_install.sh"
+  echo "[+] Usage: [Environment Variables] jq_install.sh [-hn]"
+  echo "[+]   Environment Variables:"
+  echo "[+]     LOG_PATH              path for logs (default: '/var/root/log')"
 }
 
 # Logic
 
-case "${PLATFORM}" in
-  Darwin* ) install_osx
-            exit 0
-            ;;
-  Linux* )  install_linux
-            exit 0
-            ;;
-  * )       usage
-            exit 1
-esac
+# Argument parsing
+while [ "$1" != "" ]; do
+  case $1 in
+    -n | --nolog ) unset_logs
+                   ;;
+    -h | --help )  usage
+                   exit 0
+  esac
+  shift
+done
+
+install
