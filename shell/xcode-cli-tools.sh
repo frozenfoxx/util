e4f0bede310a13a6004c1d94d07d827004b9d208
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-06-30 13:47:55 -0700

    Create xcode-cli-tools.sh

---
+++ shell/xcode-cli-tools.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Originally pulled from https://github.com/timsutton/osx-vm-templates
+# Modified by FrozenFOXX
+
+if [[ ! "$INSTALL_XCODE_CLI_TOOLS" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
+    exit
+fi
+
+# Get and install Xcode CLI tools
+OSX_VERS=$(sw_vers -productVersion | awk -F "." '{print $2}')
+
+# on 10.9+, we can leverage SUS to get the latest CLI tools
+if [ "$OSX_VERS" -ge 15 ]; then
+    # create the placeholder file that's checked by CLI updates' .dist code
+    # in Apple's SUS catalog
+    touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+    # find the CLI Tools update
+    PROD=$(softwareupdate -l | grep "\*.*Label: Command Line Tools" | tail -n 1 | awk -F"*" '{print $2}' | sed -e 's/^.*Label:.*Command/Command/' | tr -d '\n')
+    # install it
+    softwareupdate -i "$PROD" --verbose
+    rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+else
+    echo "This version is no longer supported via this script, please install manually and update"
+    exit 1
+fi
