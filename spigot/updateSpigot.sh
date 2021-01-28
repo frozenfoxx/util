f399d3b7cd00828351a1526c533fa7a9bc5e6ea9
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2021-01-27 17:14:36 -0800

    Update updateSpigot.sh

--- spigot/updateSpigot.sh
+++ spigot/updateSpigot.sh
@@ -20,16 +20,16 @@ mv ${HOME}/build/craftbukkit-*.jar ${HOME}/
 mv ${HOME}/build/spigot-*.jar ${HOME}/
 
 echo "[+] Building symlinks..."
-if [ -f ${HOME}/craftbukkit-latest.jar ]; then
-	  rm ${HOME}/craftbukkit-latest.jar
-  fi
+if [[ -f ${HOME}/craftbukkit-latest.jar ]]; then
+  rm ${HOME}/craftbukkit-latest.jar
+fi
 
-  if [ -f ${HOME}/spigot-latest.jar ]; then
-	    rm ${HOME}/spigot-latest.jar
-    fi
+if [[ -f ${HOME}/spigot-latest.jar ]]; then
+  rm ${HOME}/spigot-latest.jar
+fi
 
-    ln -s $(ls -t ${HOME}/craftbukkit-* | head -1) ${HOME}/craftbukkit-latest.jar
-    ln -s $(ls -t ${HOME}/spigot-* | head -1) ${HOME}/spigot-latest.jar
+ln -s $(ls -t ${HOME}/craftbukkit-* | head -1) ${HOME}/craftbukkit-latest.jar
+ln -s $(ls -t ${HOME}/spigot-* | head -1) ${HOME}/spigot-latest.jar
 
-    echo "[+] Restarting server..."
-    java -Xms512M -Xmx512M -XX:+UseConcMarkSweepGC -jar ${HOME}/spigot-latest.jar
+echo "[+] Restarting server..."
+java -Xms512M -Xmx512M -XX:+UseConcMarkSweepGC -jar ${HOME}/spigot-latest.jar
